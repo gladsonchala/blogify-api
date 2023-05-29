e6d6f7e1b06d4f95772d56aa2cb4d59612c35906
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-29 15:56:30 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config(); // Load environment variables from .env file
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start the server after successful database connection
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+  });
