a74db487185401e8820930d128dfb9e6423ae069
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-29 16:05:09 +0300

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -39,3 +39,35 @@ const register = async (req, res) => {
 module.exports = {
   register,
 };
+
+
+// User login
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+    // Check if the password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+    // Generate a JWT token for the authenticated user
+    const token = generateToken(user._id);
+
+    // Return the token and user details
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while logging in' });
+  }
+};
+
+module.exports = {
+  login,
+};
