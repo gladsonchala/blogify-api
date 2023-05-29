5c2052348bc3cc6a81b74c6226f9a042d9248863
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-29 17:24:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('/');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -14,7 +18,7 @@ mongoose.connect(process.env.MONGODB_URI, {
     console.log('Connected to MongoDB');
     // Start the server after successful database connection
     app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+      console.log(Server is running on port ${port});
     });
   })
   .catch((error) => {
