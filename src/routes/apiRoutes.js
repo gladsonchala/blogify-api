a716ae72048b525d5089f1e9bdfb2df47890652a
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-29 16:30:19 +0300

    Update apiRoutes.js

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const postController = require('../controllers/postController');
+const userController = require('../controllers/userController');
 
 // Authentication routes
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+
+// Blog users routes
+router.get('/users', userController.getAllUsers);
+router.get('/users/:id', userController.getUserById);
+router.put('/users/:id', authController.verifyToken, userController.updateUser);
+router.delete('/users/:id', authController.verifyToken, userController.deleteUser);
 
 // Blog post routes
 router.post('/posts', authController.verifyToken, postController.createPost);
